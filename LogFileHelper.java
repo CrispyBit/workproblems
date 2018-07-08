c64ed09f886ae7cd889a56b33779a74843bf1ae6
mattbraun425 <40967275+mattbraun425@users.noreply.github.com>
2018-07-08 15:26:48 -0700

    Create LogFileHelper.java

---
+++ LogFileHelper.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class LogFileHelper {
+
+    public List<String> sortLogData(List<String> rawLogData){
+        List<String> sortedData = null;
+
+        return sortedData;
+    }
+}
